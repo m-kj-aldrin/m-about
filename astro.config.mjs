4c71cc914c3699ab972c5b305ccf449f53fc3cfd
MKJ <mkj.aldrin@icloud.com>
2023-08-03 22:54:54 +0200

    third

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,10 @@
 import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
-  // output: "server",
-
   adapter: netlify(),
-  // adapter: node({
-  //   mode: "standalone",
-  // }),
   experimental: {
     assets: true,
   },
