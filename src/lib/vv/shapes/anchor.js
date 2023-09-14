bb2966a86ce61296e8e01949dd049ac728719aa6
MKJ <mkj.aldrin@icloud.com>
2023-09-14 21:52:07 +0200

    clean

--- src/lib/vv/shapes/anchor.js
+++
@@ -1,2 +0,0 @@
-// Handle z sort -> Get all children and grandchildren into a flatmap and sort the by a .sortValue
-// Handle transform -> transform anchor origin and all its children
--- src/lib/vv/shapes/shape.js
+++
@@ -1,2 +0,0 @@
-// extends anchor.js, might not need ... astro components implements the shape building
-// Does not render, astro renders the graph
