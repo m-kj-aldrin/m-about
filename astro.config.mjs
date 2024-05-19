d91941eb1559731e0fc3f5d89bab76abb6965e98
MKJ <mkj.aldrin@icloud.com>
2024-05-19 21:56:38 +0200

    yes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from "astro/config";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
     experimental: {
         clientPrerender: true,
     },
+    output: "hybrid",
+    adapter: netlify(),
 });
