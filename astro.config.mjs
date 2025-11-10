96486227a08d126694bcc2365a351b71242a366f
m-kj-aldrin <mkj.aldrin@icloud.com>
2025-11-10 11:19:30 +0100

    correct function dir

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,7 @@ import UnoCss from "unocss/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  // prefetch: {
-  //   defaultStrategy: "load",
-  // },
-  experimental: {
-    clientPrerender: true,
-  },
   integrations: [UnoCss()],
-  output: "hybrid",
+  // output: "server",
   adapter: netlify(),
 });
